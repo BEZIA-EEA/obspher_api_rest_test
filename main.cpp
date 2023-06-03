e49f994b4aee53289b799bdf810bf33970f89caf
Ayoub BEZIA <ayoub.bezia@gmail.com>
2023-06-03 16:17:52 +0200

    coreect CMake file and add handlers codes

--- main.cpp
+++ main.cpp
@@ -5,39 +5,121 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
+#include <boost/asio.hpp>
 #include <mysql_driver.h>
 #include <mysql_connection.h>
 
 namespace http = boost::beast::http;
+namespace asio = boost::asio;
 
-// Fonction pour traiter les requêtes HTTP
+
 void handle_request(const http::request<http::string_body>& req, http::response<http::string_body>& res) {
-    // Votre logique de traitement des requêtes ici
+    boost::beast::string_view uri = req.target();
+    
+    if (req.method() == http::verb::get) {
+        if (uri == "/login") {
+            // Logique de connexion pour générer un token
+        }
+        else if (uri == "/users") {
+            // Récupération de la liste des utilisateurs
+        }
+        else if (uri.substr(0, 7) == "/users/") {
+            std::string_view user_id(uri.substr(7).data(), uri.substr(7).size());
+            // Récupération des détails d'un utilisateur avec l'ID spécifié
+        }
+        else if (uri.substr(0, 9) == "/projects") {
+            std::string_view user_id(uri.substr(9).data(), uri.substr(9).size());
+            // Récupération de la liste des projets pour un utilisateur donné
+        }
+        else if (uri.substr(0, 9) == "/projects") {
+            std::string_view project_id(uri.substr(9).data(), uri.substr(9).size());
+            // Récupération des détails d'un projet avec l'ID spécifié
+        }
+    }
+    else if (req.method() == http::verb::post || req.method() == http::verb::put) {
+        if (uri == "/users") {
+            // Ajout d'un utilisateur avec les données de la requête
+        }
+        else if (uri == "/projects") {
+            // Ajout d'un projet pour un utilisateur donné avec les données de la requête
+        }
+    }
+    else if (req.method() == http::verb::patch) {
+        if (uri.substr(0, 7) == "/users/") {
+            std::string_view user_id(uri.substr(7).data(), uri.substr(7).size());
+            // Modification d'un utilisateur avec l'ID spécifié et les données de la requête
+        }
+        else if (uri.substr(0, 9) == "/projects") {
+            std::string_view project_id(uri.substr(9).data(), uri.substr(9).size());
+            // Modification d'un projet avec l'ID spécifié et les données de la requête
+        }
+    }
+    else if (req.method() == http::verb::delete_) {
+        if (uri.substr(0, 7) == "/users/") {
+            std::string_view user_id(uri.substr(7).data(), uri.substr(7).size());
+            // Suppression d'un utilisateur avec l'ID spécifié
+        }
+        else if (uri.substr(0, 9) == "/projects") {
+            std::string_view project_id(uri.substr(9).data(), uri.substr(9).size());
+            // Suppression d'un projet avec l'ID spécifié
+        }
+    }
 }
 
-// Fonction pour créer les réponses HTTP
 void create_response(const http::request<http::string_body>& req, http::response<http::string_body>& res) {
-    // Votre logique de création des réponses HTTP ici
+    res.version(req.version());
+    res.keep_alive(false);
+    
+    // Ajout des en-têtes de la réponse
+    res.set(http::field::server, "MonServeur/1.0");
+    res.set(http::field::content_type, "application/json");
+    
+    // Génération du contenu de la réponse
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    rapidjson::Document responseJson;
+    
+    // Remplir le document JSON de la réponse en fonction de la requête et du résultat obtenu
+    
+    // Convertir le document JSON en chaîne de caractères
+    responseJson.Accept(writer);
+    
+    // Définir le corps de la réponse
+    res.body() = buffer.GetString();
+    
+    // Définir la longueur du corps de la réponse
+    res.content_length(res.body().size());
 }
+
 
 int main() {
     try {
-        // Code pour initialiser le serveur et l'écoute des requêtes
+        // Initialisation du serveur
+        asio::io_context io_context;
+        asio::ip::tcp::acceptor acceptor(io_context, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), 8080));
         
-        // Boucle principale pour traiter les requêtes
         while (true) {
-            // Code pour accepter et gérer les connexions
+            // Attente et gestion des connexions
+            asio::ip::tcp::socket socket(io_context);
+            acceptor.accept(socket);
             
-            // Code pour lire les requêtes HTTP entrantes
+            // Lecture de la requête HTTP
+            boost::beast::flat_buffer buffer;
+            http::request<http::string_body> request;
+            http::read(socket, buffer, request);
             
-            // Code pour appeler la fonction de traitement des requêtes
+            // Traitement de la requête
             http::response<http::string_body> response;
             handle_request(request, response);
             
-            // Code pour créer la réponse HTTP
+            // Création de la réponse HTTP
             create_response(request, response);
             
-            // Code pour envoyer la réponse HTTP
+            // Envoi de la réponse HTTP
+            http::write(socket, response);
+            
+            // Fermeture de la connexion
+            socket.shutdown(boost::asio::ip::tcp::socket::shutdown_send);
         }
     } catch (const std::exception& e) {
         std::cerr << "Erreur : " << e.what() << std::endl;
